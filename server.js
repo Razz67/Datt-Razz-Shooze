a25e8b7ed679a5d7fd1c8d9c18d490843103a7ca
Razz67 <srasberry00@gmail.com>
2022-10-03 10:22:14 -0400

    Finish models

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const mongoose = require('mongoose');
 const mongoConfig = require('./config');
+const path = require('path');
 const jwt = require('jsonwebtoken');
+const app = express();
+const port = process.env.PORT || 3000;
+
 
 
 require('dotenv').config();
-const app = express();
-const port = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
 
 // Set up view engine
 app.set('view engine', 'ejs');
@@ -15,13 +17,19 @@ app.engine("jsx", require("express-react-views").createEngine());
 
 // ********** Middleware **********
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.use(express.json());
+
+// serve client files
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 // listen on port 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
 // Connect to MongoDB 
 mongoConfig();
-
