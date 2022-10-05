3dc6f51ea0c7303ec83bc05bd6e1936997d3d2f3
Razz67 <srasberry00@gmail.com>
2022-10-05 16:24:55 -0400

    Fixed syntax issue

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 	res.redirect("/products");
-}")
+});
 // app.use(express.static("images"));
 
 app.use("/products", productsRoute);
